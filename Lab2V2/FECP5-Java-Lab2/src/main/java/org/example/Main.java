17fb3e6eab7118fafd53130007a7c0bc863e051b
Tyrell Teppang <tyrellmarcus.teppang@gcash.com>
2025-06-30 16:29:27 +0800

    Second coomit; almost done

---
+++ Lab2V2/FECP5-Java-Lab2/src/main/java/org/example/Main.java
@@ -0,0 +1,64 @@
+package org.example;
+import java.util.*;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Enter membership status (Regular, VIP, Premium): ");
+        String userMembership = scanner.nextLine();
+
+        System.out.printf("Enter age: ");
+        int userAge = scanner.nextInt();
+
+        float membershipPrice = 0;
+
+        switch(userMembership.toUpperCase()){
+            case "REGULAR":
+
+                if (userAge < 18){
+                    membershipPrice = 50.0f;
+                }
+                else if (userAge >= 18 && userAge <= 64){
+                    membershipPrice = 100.0f;
+                }
+                else if (userAge >64){
+                    membershipPrice = 75.0f;
+                }
+                else {
+                    System.out.printf("Invalid Membership Status! ");
+                }
+
+                break;
+            case "VIP":
+
+                if (userAge < 18){
+                    membershipPrice = 75.0f;
+                }
+                else if (userAge >= 18 && userAge <= 64){
+                    membershipPrice = 150.0f;
+                }
+                else if (userAge >64){
+                    membershipPrice = 112.5f;
+                }
+
+                break;
+            case "PREMIUM":
+
+                if (userAge < 18){
+                    membershipPrice = 100.0f;
+                }
+                else if (userAge >= 18 && userAge <= 64){
+                    membershipPrice = 200.0f;
+                }
+                else if (userAge >64){
+                    membershipPrice = 150.0f;
+                }
+
+                break;
+        }
+
+        System.out.printf("Price: $%.2f", membershipPrice);
+    }
+}
